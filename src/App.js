17991f5b3771a41b21d696f878ae6434a582576f
Alfurquan Zahedi <alzahedi@microsoft.com>
2024-07-17 20:13:05 +0530

    Added extensions

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!
+          Learn React easily!
         </a>
       </header>
     </div>
