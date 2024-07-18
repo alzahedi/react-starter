e019581e560da8eb314237332c80f6861e863ef1
Alfurquan Zahedi <alzahedi@microsoft.com>
2024-07-18 10:43:26 +0530

    Settings updated

--- .devcontainer/scripts/post-create-commands.sh
+++ .devcontainer/scripts/post-create-commands.sh
@@ -2,9 +2,4 @@ echo "Installing dependencies"
 
 npm install
 
-echo "Setting up git creds"
-
-git config --global user.name $GIT_USERNAME
-git config --global user.email $GIT_EMAIL
-
 echo "Post-create script completed!"
